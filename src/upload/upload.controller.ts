f9e786c9a5ad2df3d5b089bf80103de128b65fbc
Michel França <fr.mich17@gmail.com>
2024-02-03 03:27:17 -0300

    feat: receber os arquivos enviados pelos clientes

---
+++ src/upload/upload.controller.ts
@@ -0,0 +1,22 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('upload')
+export class UploadController {
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(
+    @UploadedFile()
+    file: Express.Multer.File,
+  ) {
+    return {
+      message: 'Arquivo enviado.',
+      file,
+    };
+  }
+}
